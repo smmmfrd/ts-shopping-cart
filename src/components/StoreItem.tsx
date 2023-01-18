ecbc681680482976973f2093ff90e53ce574d609
smmmfrd <smmmfrd@gmail.com>
2023-01-18 11:45:30 -0800

    Item Buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProp = {
@@ -8,15 +8,31 @@ type StoreItemProp = {
     imgUrl: string
 }
 
-export function StoreItem({id, name, price, imgUrl}: StoreItemProp) {
-    return(
-        <Card>
-            <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit: "cover"}} />
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProp) {
+    const quantity: number = 1;
+
+    return (
+        <Card className="h-100">
+            <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }} />
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                     <span className="fs-2">{name}</span>
                     <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">+ Add To Cart</Button>
+                    ) : <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem" }}>
+                        <div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem" }}>
+                            <Button>-</Button>
+                            <div>
+                                <span className="fs-3">{quantity}</span> in cart
+                            </div>
+                            <Button>+</Button>
+                        </div>
+                        <Button variant="danger" size="sm">Remove</Button>
+                    </div>}
+                </div>
             </Card.Body>
         </Card>
     )
