8b84b71a0f0372d22eab07ec6b20f87522db885c
smmmfrd <smmmfrd@gmail.com>
2023-01-18 12:53:10 -0800

    Shopping Cart Display

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -80,6 +81,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
             value={{ openCart, closeCart, getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity }}
         >
             {children}
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     );
 }
